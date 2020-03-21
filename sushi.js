ed95127d83ac1e80e586b6cb451cb9e2a74333b1
matsuyoshi <sfbgwm30@gmail.com>
2020-03-21 13:36:43 +0900

    寿司増殖 #3

--- sushi.js
+++ sushi.js
@@ -1,6 +1,4 @@
 'use strict';
-(function () {
-
 const WIDTH = document.getElementById('myCanvas').clientWidth;
 const HEIGHT = document.getElementById('myCanvas').clientHeight;
 // console.log('WIDTH is ' + WIDTH + ', HEIGHT is ' + HEIGHT);
@@ -10,34 +8,49 @@ canvas.width = WIDTH;
 canvas.height = HEIGHT;
 const ctx = canvas.getContext('2d');
 ctx.font = '30px sans-serif';
-
 document.getElementById('myCanvas').appendChild(canvas);
 
-var sushi = {
-    x: 80,
-    y: 40,
-    vx: 5,
-    vy: 2,
-    image: '🍣',
-    draw: function() {
+class Sushi {
+    constructor(x, y, vx, vy) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.image = '🍣';
+    }
+    update() {
+        if(0 > this.x || this.x > WIDTH-30) {
+            this.vx = this.vx * -1;
+        }
+        this.x += this.vx;
+        if(30 > this.y || this.y > HEIGHT) {
+            this.vy = this.vy * -1;
+        }
+        this.y += this.vy;
+    }
+    draw(ctx) {
         ctx.fillText(this.image, this.x, this.y);
     }
 }
 
-function loop() {
-    ctx.clearRect(0,0, WIDTH, HEIGHT);
-    sushi.x += sushi.vx;
-    sushi.y += sushi.vy;
-    if (0 > sushi.x || sushi.x > WIDTH-30) {
-        sushi.vx = sushi.vx * -1;
-    }
-    if (30 > sushi.y || sushi.y > HEIGHT) {
-        sushi.vy = sushi.vy * -1;
-    }
-    sushi.draw();
-    requestAnimationFrame(loop);
+function getRandomInt(max, min) {
+    max = Math.floor(max);
+    min = Math.floor(min);
+    return Math.floor((Math.random() * (max-min)) + min);
 }
 
-loop();
+const objects = [];
+objects.push(new Sushi(80, 40, 5, 2));
 
-}) ();
+function loop(timestamp) {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    objects.forEach((obj) => obj.update());
+    objects.forEach((obj) => obj.draw(ctx));
+    window.requestAnimationFrame((ts) => loop(ts));
+}
+
+window.requestAnimationFrame((ts) => loop(ts));
+
+window.addEventListener('click', event => {
+    objects.push(new Sushi(event.x, event.y, getRandomInt(5, 1), getRandomInt(5, 1)));
+});
