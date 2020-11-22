402139d9f2779f5d07fbf28fe78c8f379089b3f5
matsuyoshi <sfbgwm30@gmail.com>
2020-11-22 15:21:31 +0900

    Add eslint and prettier

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,41 +1,54 @@
-import Sushi from "./Sushi"
+import Sushi from "./Sushi";
 
 export default class Controller {
-    private context: CanvasRenderingContext2D
-    private objects: Sushi[]
-    private sushiFlag: boolean
+  private context: CanvasRenderingContext2D;
+  private objects: Sushi[];
+  private sushiFlag: boolean;
 
-    constructor(canvas: HTMLCanvasElement, private wrapper: HTMLDivElement, private img: HTMLImageElement) {
-        this.context = <CanvasRenderingContext2D>canvas.getContext('2d')
-        canvas.setAttribute('width', `${wrapper.offsetWidth}`)
-        canvas.setAttribute('height', `${wrapper.offsetHeight}`)
-        this.objects = []
-        this.sushiFlag = true
+  constructor(
+    canvas: HTMLCanvasElement,
+    private wrapper: HTMLDivElement,
+    private img: HTMLImageElement
+  ) {
+    this.context = <CanvasRenderingContext2D>canvas.getContext("2d");
+    canvas.setAttribute("width", `${wrapper.offsetWidth}`);
+    canvas.setAttribute("height", `${wrapper.offsetHeight}`);
+    this.objects = [];
+    this.sushiFlag = true;
 
-        this.render()
-    }
+    this.render();
+  }
 
-    static start(canvas: HTMLCanvasElement, wrapper: HTMLDivElement, img: HTMLImageElement) {
-        new Controller(canvas, wrapper, img).start();
-    }
+  static start(
+    canvas: HTMLCanvasElement,
+    wrapper: HTMLDivElement,
+    img: HTMLImageElement
+  ) {
+    new Controller(canvas, wrapper, img).start();
+  }
 
-    public start() {
-        this.addSushi(new MouseEvent("initial", undefined));
-        this.wrapper.onclick = e => this.addSushi(e);
-        this.img.onclick = () => {
-            this.objects.forEach((obj) => obj.changeImage())
-            this.sushiFlag = !this.sushiFlag
-        }
-    }
+  public start() {
+    this.addSushi(new MouseEvent("initial", undefined));
+    this.wrapper.onclick = (e) => this.addSushi(e);
+    this.img.onclick = () => {
+      this.objects.forEach((obj) => obj.changeImage());
+      this.sushiFlag = !this.sushiFlag;
+    };
+  }
 
-    private render() {
-        this.context.clearRect(0, 0, this.wrapper.offsetWidth, this.wrapper.offsetHeight)
-        this.objects.forEach((obj) => obj.move())
-        this.objects.forEach((obj) => obj.draw(this.context))
-        requestAnimationFrame((ts) => this.render());
-    }
+  private render() {
+    this.context.clearRect(
+      0,
+      0,
+      this.wrapper.offsetWidth,
+      this.wrapper.offsetHeight
+    );
+    this.objects.forEach((obj) => obj.move());
+    this.objects.forEach((obj) => obj.draw(this.context));
+    requestAnimationFrame((ts) => this.render());
+  }
 
-    private addSushi(e: MouseEvent) {
-        this.objects.push(new Sushi(e, this.wrapper, this.context, this.sushiFlag));
-    }
+  private addSushi(e: MouseEvent) {
+    this.objects.push(new Sushi(e, this.wrapper, this.context, this.sushiFlag));
+  }
 }
--- src/Sushi.ts
+++ src/Sushi.ts
@@ -1,41 +1,48 @@
 export default class Sushi {
-    private x: number;
-    private y: number;
-    private vx: number;
-    private vy: number;
-    private image: string;
+  private x: number;
+  private y: number;
+  private vx: number;
+  private vy: number;
+  private image: string;
 
-    constructor(private e: MouseEvent, private wrapper: HTMLDivElement, private context: CanvasRenderingContext2D, sushiFlag: boolean) {
-        e.type == "initial" ? this.x = 300 : this.x = e.clientX;
-        e.type == "initial" ? this.y = 400 : this.y = e.clientY;
-        this.vx = this.getRandomInt(5, -5)
-        this.vy = this.getRandomInt(5, -5)
-        sushiFlag ? this.image = '🍣' : this.image = '☕'
-        this.context.font = '30px sans-serif'
+  constructor(
+    private e: MouseEvent,
+    private wrapper: HTMLDivElement,
+    private context: CanvasRenderingContext2D,
+    sushiFlag: boolean
+  ) {
+    e.type == "initial" ? (this.x = 300) : (this.x = e.clientX);
+    e.type == "initial" ? (this.y = 400) : (this.y = e.clientY);
+    this.vx = this.getRandomInt(5, -5);
+    this.vy = this.getRandomInt(5, -5);
+    sushiFlag ? (this.image = "🍣") : (this.image = "☕");
+    this.context.font = "30px sans-serif";
 
-        requestAnimationFrame(this.move.bind(this))
+    requestAnimationFrame(this.move.bind(this));
+  }
+
+  public move() {
+    if (0 > this.x || this.x > this.wrapper.offsetWidth - 30) {
+      this.vx = this.vx * -1;
     }
-
-    public move() {
-        if (0 > this.x || this.x > this.wrapper.offsetWidth - 30) {
-            this.vx = this.vx * -1;
-        }
-        this.x += this.vx;
-        if (30 > this.y || this.y > this.wrapper.offsetHeight) {
-            this.vy = this.vy * -1;
-        }
-        this.y += this.vy;
+    this.x += this.vx;
+    if (30 > this.y || this.y > this.wrapper.offsetHeight) {
+      this.vy = this.vy * -1;
     }
+    this.y += this.vy;
+  }
 
-    public changeImage() {
-        this.image == '🍣' ? this.image = '☕' : this.image = '🍣';
-    }
+  public changeImage() {
+    this.image == "🍣" ? (this.image = "☕") : (this.image = "🍣");
+  }
 
-    public draw(context: CanvasRenderingContext2D) {
-        context.fillText(this.image, this.x, this.y);
-    }
+  public draw(context: CanvasRenderingContext2D) {
+    context.fillText(this.image, this.x, this.y);
+  }
 
-    private getRandomInt(max:number, min:number) {
-        return Math.floor((Math.random() * (Math.floor(max) - Math.floor(min))) + Math.floor(min));
-    }
+  private getRandomInt(max: number, min: number) {
+    return Math.floor(
+      Math.random() * (Math.floor(max) - Math.floor(min)) + Math.floor(min)
+    );
+  }
 }
